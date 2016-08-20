c55634a8c4ff9f27f1efe7f47e59b7cf97d59800
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-08-20 16:17:15 -0700

    hexi: export math crate under `hexi::math`

--- hexi/src/lib.rs
+++ hexi/src/lib.rs
@@ -1,6 +1,9 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+/// Vec2 is a generic two-component vector type.
+///
+/// # Examples
+///
+/// ```
+/// let x = hexi::math::Vec2(4.0f32, 8.0f32);
+/// println!("{:?}", x);
+/// ```
+pub extern crate hexi_math as math;
