ed5072a4aa2ebf4fddc3dbd572eae884be8da98d
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2016-08-21 19:46:07 -0700

    math: add Point2 type

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -1,7 +1,9 @@
 mod vec2;
 mod vec3;
 mod vec4;
+mod point2;
 
 pub use self::vec2::*;
 pub use self::vec3::*;
 pub use self::vec4::*;
+pub use self::point2::*;
---
+++ math/src/point2.rs
@@ -0,0 +1,16 @@
+/// Point2 is a generic two-component point type.
+///
+/// # Examples
+///
+/// ```
+/// let x = hexi_math::Point2(4.0f32, 8.0f32);
+/// println!("{:?}", x);
+/// ```
+///
+/// ```
+/// let x = hexi_math::Point2(1u8, 5u8);
+/// println!("{:?}", x);
+/// println!("({:?}, {:?})", x.0, x.1);
+/// ```
+#[derive(Copy, Clone, Debug)]
+pub struct Point2<T>(pub T, pub T);
